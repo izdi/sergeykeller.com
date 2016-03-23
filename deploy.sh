dddf6afe31498943552c10840e2d5d9a597a1ff5
izdi <izdieu@gmail.com>
2016-03-24 00:30:41 +0300

    rebuilding site Thu Mar 24 00:30:41 MSK 2016

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO: improve build via args
+hugo -t hugo-zd --buildDrafts
+
+git add -A
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+git subtree push --prefix=public git@github.com:izdi/tishlbn.git gh-pages
